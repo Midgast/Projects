77d5c52d1a2015294fa71a7a5127b46cc1278581
Abubakr <abubakr@desktop-jeq2boo>
2026-01-27 15:42:32 +0600

    Delete all files from repository

--- al-tamimi-college-system/backend/src/routes/me.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { requireAuth } from "../middleware/auth.js";
-
-export const meRouter = Router();
-
-meRouter.get("/", requireAuth, async (req, res) => {
-  res.json({ user: req.user });
-});
